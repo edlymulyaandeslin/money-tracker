8652e027dba10666fd5c5bfc1fc147927f09fa45
Edly Mulya Andeslin <edlymulyaandeslin@gmail.com>
2025-09-10 23:34:48 +0700

    update: add_is_premium_to_users_table and update icon sidebar laporan keuangan

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { LayoutGrid } from 'lucide-react';
+import { Landmark, LayoutGrid } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: { label: string; items: NavItem[] }[] = [
@@ -19,7 +19,7 @@ const mainNavItems: { label: string; items: NavItem[] }[] = [
             {
                 title: 'Laporan Keuangan',
                 href: '/keuangan',
-                icon: LayoutGrid,
+                icon: Landmark,
             },
         ],
     },
@@ -30,11 +30,6 @@ const footerNavItems: NavItem[] = [
     //     title: 'Repository',
     //     href: 'https://github.com/laravel/react-starter-kit',
     //     icon: Folder,
-    // },
-    // {
-    //     title: 'Documentation',
-    //     href: 'https://laravel.com/docs/starter-kits#react',
-    //     icon: BookOpen,
     // },
 ];
 
