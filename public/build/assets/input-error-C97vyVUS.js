9602f03cb906f4dcfb84ad8b969be120de065204
Edly Mulya Andeslin <edlymulyaandeslin@gmail.com>
2025-09-12 03:26:13 +0700

    build: building css style

---
+++ public/build/assets/input-error-C97vyVUS.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-SqrRNJg2.js";import{c as o}from"./button-Bmr8kS3K.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
